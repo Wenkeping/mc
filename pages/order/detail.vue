3cc64e9010563ad2dc17fb7cd3ab6542255dd3eb
wenkeping <527999446@qq.com>
2021-04-19 23:10:04 +0800

    update

--- pages/order/detail.vue
+++ pages/order/detail.vue
@@ -1,55 +1,57 @@
 <template>
 	<view class="container">
 		<view class="header"></view>
-		<view class="content" v-if="Object.keys(order).length>0">
+		<view v-if="Object.keys(order).length > 0">
 			<view class="order-box">
 				<view class="text-color-primary font-size-lg font-weight-bold mb-40">订单已完成</view>
 				<view class="text-color-assist font-size-sm">感谢您对喜茶的支持，欢迎再次光临</view>
 			</view>
-			
-			<view class="flex-fill overflow-auto">
-				<view class="">
-					<list-cell padding="50rpx 40rpx">
-						<view class="w-100 d-flex align-items-center">
-							<view class="flex-fill d-flex flex-column overflow-hidden">
-								<view class="font-size-lg mb-10">{{ order.shop.name }}</view>
-								<view class="font-size-extra-sm text-color-assist text-truncate">{{ order.shop.address }}</view>
-							</view>
-							<view class="d-flex align-items-center ml-20">
-								<image src="/static/images/order/icon_phone.png" class="phone-icon"></image>
-								<image src="/static/images/order/icon_map.png" class="map-icon"></image>
-							</view>
+				
+			<view class="section">
+				<list-cell :hover="false" padding="50rpx 40rpx">
+					<view class="w-100 d-flex align-items-center">
+						<view class="flex-fill d-flex flex-column overflow-hidden">
+							<view class="font-size-lg mb-10">{{ order.shop.name }}</view>
+							<view class="font-size-extra-sm text-color-assist text-truncate">{{ order.shop.address }}</view>
 						</view>
-					</list-cell>
-					<list-cell padding="0 40rpx">
-						<view class="w-100 d-flex flex-column">
-							<view class="d-flex align-items-center mt-40" v-for="(item, index) in order.items" :key="index">
-								<view><image :src="item.images.data[index].url" class="pro-img"></image></view>
-								<view class="flex-fill d-flex flex-column ml-30">
-									<view class="font-size-base mb-10">{{ item.sname }}</view>
-									<view class="font-size-extra-sm text-color-assist">
-										{{ materialsText(item.materials) }}
-									</view>
-								</view>
-								<view class="flex-shrink-0 font-weight-bold ml-40">x{{ item.quantity }}</view>
-								<view class="flex-shrink-0 font-weight-bold ml-40">￥{{ item.price }}</view>
-							</view>
-							<view class="d-flex justify-content-between align-items-center mt-40 pb-30 border-dashed"></view>
-							<view class="d-flex justify-content-between align-items-center font-size-lg font-weight-bold mt-30 mb-40">
-								<view>合计</view>
-								<view>￥{{ order.total_fee }}</view>
-							</view>
+						<view class="d-flex align-items-center ml-20">
+							<image src="/static/images/order/icon_phone.png" class="phone-icon"></image>
+							<image src="/static/images/order/icon_map.png" class="map-icon"></image>
 						</view>
-					</list-cell>
-					<list-cell padding="30rpx 40rpx" last style="border-radius: 0 0 30rpx 30rpx;">
-						<view class="w-100 d-flex flex-column font-size-extra-sm text-color-assist">
-							<view class="mb-10">下单时间：{{ order.paid_at }}</view>
-							<view class="mb-10">订单编号：{{ order.no }}</view>
-							<view class="mb-10">备注信息：{{ order.remarks }}</view>
-						</view>
-					</list-cell>
-				</view>
+					</view>
+				</list-cell>
 			</view>
+			<view class="section">
+				<list-cell :hover="false" padding="0 40rpx">
+					<view class="w-100 d-flex flex-column">
+						<view class="d-flex align-items-around mt-40" v-for="(item, index) in order.items" :key="index">
+							<view><image :src="item.images.data[index].url" class="pro-img"></image></view>
+							<view class="flex-fill ml-30 w-100">
+								<view class="font-size-base mb-10">{{ item.sname }}</view>
+								<view class="font-size-extra-sm text-color-assist">
+									{{ materialsText(item.materials) }}
+								</view>
+							</view>
+							<view class="flex-fill ml-30 w-25">
+								<view class="flex-shrink-0 font-weight-bold">￥{{ item.price }}</view>
+								<view class="flex-shrink-0  mt-20 ml-70 font-size-extra-sm text-color-assist">x{{ item.quantity }}</view>
+							</view>
+						</view>
+						<view class="d-flex justify-content-between align-items-center font-size-lg font-weight-bold mt-30 mb-40">
+							<view>合计</view>
+							<view>￥{{ order.total_fee }}</view>
+						</view>
+					</view>
+				</list-cell>
+			</view>
+			
+			<list-cell :hover="false" padding="50rpx 30rpx 20rpx" last>
+				<view class="w-100 d-flex flex-column font-size-extra-sm text-color-assist">
+					<view class="mb-10">下单时间：{{ order.paid_at }}</view>
+					<view class="mb-10">订单编号：{{ order.no }}</view>
+					<view class="mb-10">备注信息：{{ order.remarks }}</view>
+				</view>
+			</list-cell>
 		</view>
 	</view>
 </template>
@@ -89,31 +91,19 @@
 }
 
 .header {
-	display: flex;
-	justify-content: flex-end;
-	align-items: center;
 	padding: 20rpx 0;
 }
 
-.content {
-	flex: 1;
-	display: flex;
-	flex-direction: column;
-	overflow: hidden;
-	
-	.order-box {
-		z-index: 10;
+.order-box {
 		height: 15vh;
 		border-radius: 30rpx 30rpx 0 0;
-		box-shadow: 0 0 20rpx 0 rgba($color: #333, $alpha: 0.1);
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		background-color: $bg-color-white;
-		flex-shrink: 0;
+		margin-bottom: 15rpx;
 	}
-}
 
 .phone-icon, .map-icon {
 	width: 64rpx;
@@ -124,15 +114,36 @@
 	margin-right: 40rpx;
 }
 
-.border-dashed {
-	border-bottom: 1rpx dashed $border-color;
-}
-
-
-
 
 .pro-img {
 	width: 150rpx;
 	height: 120rpx;
 }
+
+@mixin arch {
+		content: "";
+		position: absolute;
+		background-color: $bg-color;
+		width: 30rpx;
+		height: 30rpx;
+		bottom: -15rpx;
+		z-index: 10;
+		border-radius: 100%;
+	}
+	
+	.section {
+		position: relative;
+		
+		&::before {
+			@include arch;
+			left: -15rpx;
+		}
+		
+		&::after {
+			@include arch;
+			right: -15rpx;
+		}
+	}
+	
+	
 </style>
