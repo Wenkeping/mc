c316f333f8cc0c19775e4643c20a0bf66dc1ead1
wenkeping <527999446@qq.com>
2021-05-02 17:37:59 +0800

    update

--- components/list-cell/list-cell.vue
+++ components/list-cell/list-cell.vue
@@ -8,7 +8,7 @@
 		@tap="handleClick"
 	>
 		<slot></slot>
-		<image src="/static/images/navigator-1.png" class="arrow" v-if="arrow"></image>
+		<image src="/static/images/common/navigator-1.png" class="arrow" v-if="arrow"></image>
 		<!-- <view class="iconfont iconarrow-right arrow" v-if="arrow"></view> -->
 	</view>
 </template>
