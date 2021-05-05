aec9e02a42636bd4e0f16db5188bcb8b069eeeab
wenkeping <527999446@qq.com>
2021-05-05 18:37:13 +0800

    update

--- pages/pay/pay.vue
+++ pages/pay/pay.vue
@@ -1,95 +1,95 @@
 <template>
+	<scroll-view scroll-y scroll-with-animation class="h-100">
 	<view class="container">
-		<view>
-			<view class="payment bg-white">
-				<view class="w-100 d-flex align-items-center justify-content-between">
-					<view class="font-size-lg">送达时间</view>
-					<view class="buttons">
-						<button type="default" class="button" 
-								:class="{active: orderType == 'lunch'}" plain 
-								hover-class="none" @tap="switchOrderType">
-							中餐
-						</button>
-						<button type="default" class="button" 
-								:class="{active: orderType == 'dinner'}" plain 
-								hover-class="none" @tap="switchOrderType">
-							晚餐
-						</button>
-					</view>
+		<view class="payment bg-white">
+			<view class="w-100 d-flex align-items-center justify-content-between">
+				<view class="font-size-lg">送达时间</view>
+				<view class="buttons">
+					<button type="default" class="button" 
+							:class="{active: orderType == 'lunch'}" plain 
+							hover-class="none" @tap="switchOrderType">
+						中餐
+					</button>
+					<button type="default" class="button" 
+							:class="{active: orderType == 'dinner'}" plain 
+							hover-class="none" @tap="switchOrderType">
+						晚餐
+					</button>
 				</view>
-				<view v-if="orderType == 'lunch'" class="mt-30">成功下单后，次日 8:00-9:00 现做， 9:30-11:00 送达</view>
-				<view v-else class="mt-30">成功下单后，次日 14:00-15:00 现做, 15:30-17:00 送达</view>
 			</view>
-			
-			<navigator class="mt-20" open-type="navigate" url="/pages/pay/addresses" hover-class="none">
-				<list-cell arrow last>
-						<view class="address">
-							<view class="info"  v-if="address.userName == undefined">
-								请填写地址
+			<view v-if="orderType == 'lunch'" class="mt-30">成功下单后，次日 8:00-9:00 现做， 9:30-11:00 送达</view>
+			<view v-else class="mt-30">成功下单后，次日 14:00-15:00 现做, 15:30-17:00 送达</view>
+		</view>
+		
+		<navigator class="mt-20" open-type="navigate" url="/pages/pay/addresses" hover-class="none">
+			<list-cell arrow last>
+					<view class="address">
+						<view class="info"  v-if="address.userName == undefined">
+							请填写地址
+						</view>
+						<view class="info" v-else>
+							<view class="address-row">
+								<view class="address">{{ `${address.address} ${address.detailInfo}` }}</view>
 							</view>
-							<view class="info" v-else>
-								<view class="address-row">
-									<view class="address">{{ `${address.address} ${address.detailInfo}` }}</view>
-								</view>
-								<view class="user-row">
-									{{ `${address.userName} ${address.telNumber}` }}
-								</view>
+							<view class="user-row">
+								{{ `${address.userName} ( ${address.gender ? '女士' : '先生'} ) ${address.telNumber}` }}
 							</view>
 						</view>
-				</list-cell>
-			</navigator>
-			<view class="flex-fill overflow-auto border-radius-lg mt-20">
-				<list-cell padding="0 40rpx">
-					<view class="w-100 d-flex flex-column">
-						<view class="d-flex align-items-center mt-40" v-for="(item, index) in cart" :key="index">
-							<view><image :src="item.image" class="pro-img"></image></view>
-							<view class="flex-fill ml-30 w-100">
-								<view class="font-size-base mb-10">{{ item.name }}</view>
-								<view class="font-size-extra-sm text-color-assist">
-									<!-- {{ item.description }} -->啊嘎嘎哈哈风格和，色高山上的成功
-								</view>
-							</view>
-							<view class="flex-fill ml-30 w-25">
-								<view class="flex-shrink-0  mt-20 ml-70 font-weight-bold">￥{{ item.price }}</view>
-								<view class="flex-shrink-0  mt-20 ml-85 font-size-extra-sm text-color-assist">x{{ item.number }}</view>
-							</view>
-						</view>
-						<view class="d-flex justify-content-between align-items-center mt-40 pb-30 border-dashed"></view>
 					</view>
-				</list-cell>
+			</list-cell>
+		</navigator>
+		<view class="flex-fill overflow-auto border-radius-lg mt-20">
+			<list-cell padding="0 40rpx">
+				<view class="w-100 d-flex flex-column">
+					<view class="d-flex align-items-center mt-40" v-for="(item, index) in cart" :key="index">
+						<view><image :src="item.image" class="pro-img"></image></view>
+						<view class="flex-fill ml-30 w-100">
+							<view class="font-size-base mb-10">{{ item.name }}</view>
+							<view class="font-size-extra-sm text-color-assist">
+								<!-- {{ item.description }} -->啊嘎嘎哈哈风格和，色高山上的成功
+							</view>
+						</view>
+						<view class="flex-fill ml-30 w-25">
+							<view class="flex-shrink-0  mt-20 ml-70 font-weight-bold">￥{{ item.price }}</view>
+							<view class="flex-shrink-0  mt-20 ml-85 font-size-extra-sm text-color-assist">x{{ item.number }}</view>
+						</view>
+					</view>
+					<view class="d-flex justify-content-between align-items-center mt-40 pb-30 border-dashed"></view>
+				</view>
+			</list-cell>
 
-				<list-cell arrow last>
-					<navigator hover-class="none" class="flex-fill ml-80 text-truncate text-right" open-type="navigate" url="/pages/pay/remark">
-						<view class="w-100 d-flex align-items-center justify-content-between overflow-hidden">
-							<view  class="mr-10">备注</view>
-							{{ remark }}
-						</view>
-					</navigator>
-				</list-cell>
-				<list-cell last>
-					<view class="w-100 d-flex justify-content-end align-items-center">
-						<text class="font-size-sm">共{{ cartNum }}件商品，小计</text>
-						<text class="font-size-lg font-weight-bold">￥{{ cartAmount }}</text>
+			<list-cell arrow last>
+				<navigator hover-class="none" class="flex-fill ml-80 text-truncate text-right" open-type="navigate" url="/pages/pay/remark">
+					<view class="w-100 d-flex align-items-center justify-content-between overflow-hidden">
+						<view  class="mr-10">备注</view>
+						{{ remark }}
 					</view>
-				</list-cell>
-			</view>
-			
-			<view class="payment mt-20 mb-150 bg-white w-100 d-flex align-items-center justify-content-between">
-				<view>支付方式</view>
-				<view class="d-flex align-items-around">
-					<image src="../../static/images/pay/weichat-pay.png" class="image"></image>
-					<view class="ml-20">微信支付</view>
+				</navigator>
+			</list-cell>
+			<list-cell last>
+				<view class="w-100 d-flex justify-content-end align-items-center">
+					<text class="font-size-sm">共{{ cartNum }}件商品，小计</text>
+					<text class="font-size-lg font-weight-bold">￥{{ cartAmount }}</text>
 				</view>
-			</view>
-			
-			<view class="footer">
-				<view class="mr-30">
-					合计：<text class="font-size-lg font-weight-bold">￥{{ cartAmount }}</text>
-				</view>
-				<button type="primary" @tap="pay">支付</button>
+			</list-cell>
+		</view>
+		
+		<view class="payment mt-20 mb-150 bg-white w-100 d-flex align-items-center justify-content-between">
+			<view>支付方式</view>
+			<view class="d-flex align-items-around">
+				<image src="../../static/images/pay/weichat-pay.png" class="image"></image>
+				<view class="ml-20">微信支付</view>
 			</view>
 		</view>
+		
+		<view class="footer">
+			<view class="mr-30">
+				合计：<text class="font-size-lg font-weight-bold">￥{{ cartAmount }}</text>
+			</view>
+			<button type="primary" @tap="pay">支付</button>
+		</view>
 	</view>
+	</scroll-view>
 </template>
 
 <script>
@@ -114,8 +114,6 @@
 		},
 		data() {
 			return {
-				man:'先生',
-				women:'女士',
 				cart: uni.getStorageSync('cart'),
 				address: {
 					userName: '',
