9e243e6b14655c6d7a6901778f76b0402ed9401b
wenkeping <527999446@qq.com>
2021-05-08 16:24:26 +0800

    update

--- uniCloud-tcb/cloudfunctions/order/index.js
+++ uniCloud-tcb/cloudfunctions/order/index.js
@@ -22,21 +22,24 @@ exports.main = async (event, context) => {
 		const orderId = parseInt(Date.now() / 1000)
 		// 计算订单总金额
 		const totalFee = goodsInOrder.reduce((acc, cur) => acc + cur.number * cur.price, 0)
-		// 订单状态 1-已支付，2-烹饪中，3-派送中，4-已完成
-		const status = 1
+		// 订单状态 1-已支付，2-待烹饪，3-烹饪中，4-派送中，5-已完成
+		const status = 0
+		// 状态名称
+		const statusName = '待烹饪'
 		// 当前时间
 		const time = new Date().toLocaleDateString() +" "+ new Date().toLocaleTimeString()
 		// 生成订单
 		const orderResult = await db.collection('db-order').add({
-			openId,           // 用户openid
+			openId,        // 用户openid
 			orderId,       // 用户和时间编译的订单号
 			orderType,     // lunch-为中餐,dinner-为晚餐
 			goodsInOrder,  // 购物车列表
 			totalFee,      // 订单总价
 			chooseStore,   // 选择的门店
-			status,         // 1-为未支付,2-为已经支付
-			remark,
-			time
+			status,        // 1-已支付，2-待烹饪，3-烹饪中，4-派送中，5-已完成
+			statusName,    // 状态名称
+			remark,        // 备注
+			time           // 下单时间
 		})
 		
 		if(!orderResult.id) {
@@ -52,9 +55,9 @@ exports.main = async (event, context) => {
 	}else if(event.action == 'getOrder'){
 		let result = {}
 		// 当前订单
-		const currentOrder = await db.collection('db-order').where({openId:openId,status:{"$lt":4}}).get()
+		const currentOrder = await db.collection('db-order').where({openId:openId,status:{"$lt":3}}).orderBy("time", "desc").get()
 		// 历史订单
-		const historyOrder = await db.collection('db-order').where({openId:openId,status:4}).get()
+		const historyOrder = await db.collection('db-order').where({openId:openId,status:3}).orderBy("time", "desc").get()
 		
 		result.currentOrder = currentOrder.data
 		result.historyOrder = historyOrder.data
